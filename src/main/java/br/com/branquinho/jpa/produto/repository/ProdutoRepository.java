f794428923262a4e522586dff24fa25997b7bae2
Eduardo Branquinho <eduardo.branquinho@bluesoft.com.br>
2020-07-06 20:10:37 -0300

    criando uma query com criteria

--- src/main/java/br/com/branquinho/jpa/produto/repository/ProdutoRepository.java
+++ src/main/java/br/com/branquinho/jpa/produto/repository/ProdutoRepository.java
@@ -8,6 +8,11 @@ import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.branquinho.jpa.produto.model.Produto;
 import br.com.branquinho.jpa.produto.model.filter.ProdutoFilter;
@@ -26,6 +31,7 @@ public class ProdutoRepository {
         return Optional.ofNullable(entityManager.find(Produto.class, codigo));
     }
 
+    //exemplo de jpql
     public List<Produto> listarProdutos(ProdutoFilter filtro) {
         StringBuilder jpql = new StringBuilder("select p from Produto p ");
 
@@ -43,5 +49,25 @@ public class ProdutoRepository {
         }
 
         return query.getResultList();
+    }
+
+    //examplo de criteria
+    public List<Produto> listarProdutosPorCriteria(ProdutoFilter filtro) {
+        final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+
+        final Root<Produto> root = query.from(Produto.class);
+        final Path<String> descricao = root.get("descricao");
+        final Path<String> codigo = root.get("codigo");
+        final Path<String> categoria = root.join("categorias").<String>get("nome");
+
+        final Predicate predicateDescricao = criteriaBuilder.like(descricao, filtro.getDescricao());
+        final Predicate predicateCategoria = criteriaBuilder.equal(categoria, filtro.getCategoria());
+        final Predicate predicateCodigo = criteriaBuilder.equal(codigo, filtro.getCodigo());
+
+        query.where(predicateDescricao, predicateCategoria, predicateCodigo);
+
+        final TypedQuery<Produto> typedQuery = entityManager.createQuery(query);
+        return typedQuery.getResultList();
     }
 }
